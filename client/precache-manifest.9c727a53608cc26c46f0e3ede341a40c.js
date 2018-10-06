524e4eda0926383a5061c8e0c084fc74f36c8c14
koru <glassonyou@gmail.com>
2018-10-06 10:37:16 +0000

    client integrated build

---
+++ client/precache-manifest.9c727a53608cc26c46f0e3ede341a40c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "df13646d01fe09a99d3e",
+    "url": "/js/chunk-vendors.7453e7ab.js"
+  },
+  {
+    "revision": "386e25d03012b5ca7237",
+    "url": "/js/app.a48873fb.js"
+  },
+  {
+    "revision": "5ec49e6dccb2ca4932e9",
+    "url": "/js/about.07b90307.js"
+  },
+  {
+    "revision": "05abb4313d4992bbada3e6006028e1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e055714d2c647827907bbf144cf409f",
+    "url": "/img/logo.5e055714.png"
+  },
+  {
+    "revision": "df13646d01fe09a99d3e",
+    "url": "/css/chunk-vendors.0b70f2a9.css"
+  },
+  {
+    "revision": "386e25d03012b5ca7237",
+    "url": "/css/app.609d1941.css"
+  }
+];
