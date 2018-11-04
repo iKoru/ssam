ee9df20e5dd9c8a15c4bba265999bf7b34923872
iKoru <glassonyou@gmail.com>
2018-11-04 18:13:05 +0900

    static file 접근 path 수정

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ const router = require('./server/router');
 process.env.NODE_ENV = (process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production') ? 'production' : 'development';
 
 app.use(logger(process.env.NODE_ENV));
+app.use('/static', express.static(path.resolve(__dirname, 'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
-app.use(express.static(path.resolve(__dirname, 'client')));
 
 router(app); //take over to router
 
-server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function() {
+server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function () {
     const addr = server.address();
     console.log("ssam server listening at", addr.address + ":" + addr.port);
 });
