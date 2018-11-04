28fbf6365cff6e6c76470e17aa3f949c0bc3743d
iKoru <glassonyou@gmail.com>
2018-11-04 15:07:47 +0900

    token refresh 추가

--- server/routes/signRouter.js
+++ server/routes/signRouter.js
@@ -1,4 +1,5 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'),
+    jwt_decode = require('jwt-decode');
 const config = require('../../config');
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
@@ -8,7 +9,7 @@ module.exports = function() {
         if (req.userObject) {
             res.json({ status: "NORMAL", ...req.userObject });
         } else {
-            res.status(400);
+            res.status(400).end();
         }
     });
 
@@ -20,6 +21,28 @@ module.exports = function() {
         } else {
             res.status(400).json({ message: '잘못된 접근입니다.' });
         }
+    });
+
+    router.post('/refresh', function(req, res) {
+        const token = req.headers['x-auth'];
+        jwt.verify(token, config.jwtKey, config.jwtOptions, (err, result) => {
+            if (err && err.name === 'TokenExpiredError') { //token is valid except it's expired
+                if ((new Date(err.expiredAt).getTime() + 3600000) >= (new Date().getTime())) { //기한 만료 및 만료시간부터 1시간이 지나지 않았다면 토큰 리프레시
+                    const decoded = jwt_decode(token);
+                    if (decoded.userId) {
+                        res.json(jwt.sign({ userId: decoded.userId }, config.jwtKey, { expiresIn: (decoded.exp - decoded.iat), ...config.jwtOptions }));
+                    } else {
+                        res.status(400).json({ message: '잘못된 접근입니다.' });
+                    }
+                } else {
+                    res.status(400).end();
+                }
+            } else if (!err) { //아직 유효한 토큰이면 그대로 보낸다.
+                res.json(token);
+            } else { //기한만료가 아닌 기타 오류는 갱신 대상 아님
+                res.status(400).end();
+            }
+        });
     });
 
     return router;
