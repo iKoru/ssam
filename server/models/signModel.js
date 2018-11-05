32cc674aa86605f5d907d5a729baf64ba3f66757
iKoru <glassonyou@gmail.com>
2018-11-05 23:45:54 +0900

    model - in progress(1)

--- server/models/signModel.js
+++ server/models/signModel.js
@@ -1,9 +1,9 @@
 const pool = require('./db');
 
-exports.createSigninLog = async(user) => {
-
+exports.createSigninLog = async(userId, ip, isSuccess) => {
+    await pool.executeQuery('INSERT INTO SS_HST_USER_SIGNIN(USER_ID, SIGNIN_TIMESTAMP, IP, IS_SUCCESS) VALUES($1, current_timestamp, $2, $3)', [userId, ip, isSuccess]);
 }
 
 exports.getSigninLog = async(userId, from, to) => {
-
+    return await pool.executeQuery('SELECT * FROM SS_HST_USER_SIGNIN WHERE USER_ID = $1 AND SIGNIN_TIMESTAMP BETWEEN TO_TIMESTAMP($2) AND TO_TIMESTAMP($3)', [userId, from.getTime() / 1000, to.getTime() / 1000]);
 }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,41 +1,41 @@
 const pool = require('./db');
 
-exports.checkUserId = async (userId) => {
+exports.checkUserId = async(userId) => {
+    return await pool.executeQuery('SELECT COUNT(*) count FROM SS_MST_USER WHERE USER_ID = $1 LIMIT 1', [userId]);
+}
+
+exports.checkNickName = async(userId, nickName) => {
+    return await pool.executeQuery('SELECT COUNT(*) count FROM SS_MST_USER WHERE (LOUNGE_NICKNAME = $1 OR TOPIC_NICKNAME = $1) AND USER_ID <> $2 LIMIT 1', [nickName, userId]);
+}
+
+exports.checkEmail = async(email) => {
+    return await pool.executeQuery('SELECT COUNT(*) count FROM SS_MST_USER WHERE EMAIL = $1 LIMIT 1', [email]);
+}
+
+exports.createUser = async(user) => {
 
 }
 
-exports.checkNickName = async (nickName) => {
+exports.updateUser = async(user) => {
 
 }
 
-exports.checkEmail = async (email) => {
+exports.deleteUser = async(user) => {
 
 }
 
-exports.createUser = async (user) => {
+exports.getUser = async(userId) => {
 
 }
 
-exports.updateUser = async (user) => {
+exports.resetPassword = async(userId, email) => {
 
 }
 
-exports.deleteUser = async (user) => {
+exports.getUsers = async(userId, nickName, email, groupId, status, sortTarget = "userId", sortType = "desc", page = 1) => {
 
 }
 
-exports.getUser = async (userId) => {
-
-}
-
-exports.resetPassword = async (userId, email) => {
-
-}
-
-exports.getUsers = async (userId, nickName, email, groupId, status, sortTarget = "userId", sortType = "desc", page = 1) => {
-
-}
-
-exports.getProfile = async (nickName) => {
+exports.getProfile = async(nickName) => {
 
 }
