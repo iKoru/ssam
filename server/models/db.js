43700ed25fd73d5c48cfc67c9e866c88dd720770
iKoru <glassonyou@gmail.com>
2018-11-05 23:45:04 +0900

    common db connection handler

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,25 @@
 const { Pool } = require('pg'),
-    config = require('../../config'),
-    pool = new Pool(config.dbOptions);
+    config = require('../../config'), { partialUUID } = require('../../util'),
+    pool = new Pool(config.dbOptions),
+    logger = require('../logger');
+
+pool.on('error', (err) => {
+    logger.error('UNEXPECTED ERROR ON IDLE CLIENT', err);
+});
+
+pool.executeQuery = async(query, parameters, callback) => {
+    try {
+        const client = await pool.connect(),
+            res = null;
+        try {
+            res = await client.query({ name: partialUUID(), text: query, values: parameters }, callback)
+            logger.log(`EXECUTING QUERY : ${query}, ${parameters}`);
+        } finally {
+            client.release();
+        }
+        return res;
+    } catch (e) {
+        logger.error('EXECUTING QUERY ERROR : ', e.stack);
+    }
+}
 module.exports = pool;
