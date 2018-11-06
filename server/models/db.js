c9285a82066a94043c36b9f3fb10aeba51cd3c11
koru <glassonyou@gmail.com>
2018-11-06 01:32:43 +0000

    DB EXCEPTION LOGGING added

--- server/models/db.js
+++ server/models/db.js
@@ -9,17 +9,24 @@ pool.on('error', (err) => {
 
 pool.executeQuery = async(query, parameters, callback) => {
     try {
-        const client = await pool.connect(),
-            res = null;
+        const client = await pool.connect();
+        let res = null;
         try {
             res = await client.query({ name: partialUUID(), text: query, values: parameters }, callback)
             logger.log(`EXECUTING QUERY : ${query}, ${parameters}`);
         } finally {
             client.release();
         }
+        switch(res.command){
+            case 'SELECT':
+                return res.rows;
+            default:
+                return res;
+        }
         return res;
     } catch (e) {
         logger.error('EXECUTING QUERY ERROR : ', e.stack);
+        logger.error('TRIED QUERY TO EXECUTE : ', query, parameters);
     }
 }
 module.exports = pool;
