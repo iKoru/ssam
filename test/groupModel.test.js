2e65ca363df6befe03953d9413dd9678d3bf088f
koru <glassonyou@gmail.com>
2018-11-07 08:46:25 +0000

    insert, select, delete groups test

---
+++ test/groupModel.test.js
@@ -0,0 +1,75 @@
+/* global expect */
+const groupModel = require('../server/models/groupModel');
+
+test('insert group', async() => {
+    expect(await groupModel.createGroup({
+        groupName: '테스트 그룹',
+        groupDescription: '테스트 그룹입니다.',
+        groupType:'N',
+        orderNumber:1,
+        isOpenToUsers:false,
+        expirePeriod:-1
+    })).toEqual(1);
+    expect(await groupModel.createGroup({
+        groupName: '테스트 전공',
+        groupDescription: '테스트 전공입니다.',
+        groupType:'M',
+        orderNumber:2,
+        isOpenToUsers:false,
+        expirePeriod:-1
+    })).toEqual(1);
+    expect(await groupModel.createGroup({
+        groupName: '테스트 학년',
+        groupDescription: '테스트 학년입니다.',
+        groupType:'G',
+        orderNumber:3,
+        isOpenToUsers:false,
+        expirePeriod:-1
+    })).toEqual(1);
+    expect(await groupModel.createGroup({
+        groupName: '테스트 지역',
+        groupDescription: '테스트 지역입니다.',
+        groupType:'R',
+        orderNumber:4,
+        isOpenToUsers:false,
+        expirePeriod:-1
+    })).toEqual(1);
+});
+
+test('get groups and group by group id', async() => {
+    expect(await groupModel.getGroups(false, ['N', 'M', 'G', 'R'], 1)).toHaveLength(0);
+    expect(await groupModel.getGroups(true, ['N', 'M', 'G', 'R'], 2)).toHaveLength(0);
+    expect(await groupModel.getGroups(true, ['N'], 1)).toHaveLength(2);
+    expect(await groupModel.getGroups(true, ['M'], 1)).toHaveLength(2);
+    expect(await groupModel.getGroups(true, ['G'], 1)).toHaveLength(2);
+    expect(await groupModel.getGroups(true, ['R'], 1)).toHaveLength(2);
+    let groups = await groupModel.getGroups(true, ['N', 'M', 'G', 'R'], 1);
+    expect(groups).toHaveLength(8);
+    groups.forEach(async (group) => {
+        let dbGroup = (await groupModel.getGroup(group.groupId));
+        expect(dbGroup).toHaveLength(1);
+        dbGroup = dbGroup[0];
+        expect(dbGroup).toEqual(group);
+    });
+});
+
+test('update group', async() => {
+    
+});
+
+test('delete group', async() => {
+    let groups = await groupModel.getGroups(true, ['N', 'M', 'G', 'R'], 1);
+    groups.forEach(async (group) => {
+        if(group.groupId > 42){
+            expect(await groupModel.deleteGroup(group.groupId)).toEqual(1); 
+        }
+    });
+});
+
+test('create user group', async () => {
+    
+});
+
+test('delete user group', async () => {
+    
+});
