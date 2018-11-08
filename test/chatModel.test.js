8dc94bc9a5f9f4dad3e99df5c506a239e587bfbc
iKoru <glassonyou@gmail.com>
2018-11-08 22:24:39 +0900

    removed unnecessary test case

--- test/chatModel.test.js
+++ test/chatModel.test.js
@@ -3,16 +3,16 @@ const chatModel = require('../server/models/chatModel');
 const util = require('../server/util');
 
 test('get chats', async() => {
-   expect(await chatModel.getChats('orange')).toHaveLength(1);
-   expect(await chatModel.getChats()).toHaveLength(0);
-   expect(await chatModel.getChats('blue')).toHaveLength(1);
-   expect(await chatModel.getChats('orange', 'blue')).toHaveLength(1);
-   expect(await chatModel.getChats('orange', 'orange2')).toHaveLength(0);
-   expect(await chatModel.getChats('blue2', 'orange2')).toHaveLength(0);
+    expect(await chatModel.getChats('orange')).toHaveLength(1);
+    expect(await chatModel.getChats()).toHaveLength(0);
+    expect(await chatModel.getChats('blue')).toHaveLength(1);
+    expect(await chatModel.getChats('orange', 'blue')).toHaveLength(1);
+    expect(await chatModel.getChats('orange', 'orange2')).toHaveLength(0);
+    expect(await chatModel.getChats('blue2', 'orange2')).toHaveLength(0);
 });
 
 test('create chat', async() => {
-   expect(await chatModel.createChat('orange', 'blue', 'LOUNGE')).toEqual(1);
+    expect(await chatModel.createChat('orange', 'blue', 'LOUNGE')).toEqual(1);
 });
 
 test('get chat', async() => {
@@ -32,21 +32,17 @@ test('update chat', async() => {
 
 test('create and get message', async() => {
     const chat = await chatModel.getChats('orange', 'blue');
-    expect(await chatModel.createMessage(chat[0].chatId, 'orange', 'hello from orange')).toEqual(1); 
-    expect(await chatModel.createMessage(chat[0].chatId, 'blue', 'hello from blue')).toEqual(1); 
+    expect(await chatModel.createMessage(chat[0].chatId, 'orange', 'hello from orange')).toEqual(1);
+    expect(await chatModel.createMessage(chat[0].chatId, 'blue', 'hello from blue')).toEqual(1);
     expect((await chatModel.getMessages(chat[0].chatId, util.getYYYYMMDDHH24MISS())).length).toBeGreaterThan(1);
-});
-
-test('get message', async() => {
-   //const chat = await chatModel.getChats('orange', 'blue');
 });
 
 test('delete Chat', async() => {
     expect(await chatModel.createChat('orange', 'blue', 'TOPIC')).toEqual(1);
     const chat = await chatModel.getChats('orange', 'blue');
     expect(chat.length).toBeGreaterThan(1);
-    chat.forEach(async (c)=>{
-        if(c.chatId > 1){
+    chat.forEach(async(c) => {
+        if (c.chatId > 1) {
             expect(await chatModel.deleteChat(c.chatId)).toBeGreaterThan(0);
         }
     })
