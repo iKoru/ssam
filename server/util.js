911e3429a76b9ac3a05656268bd6b650aa5bd135
koru <glassonyou@gmail.com>
2018-11-08 06:38:26 +0000

    moment timezone included

--- server/util.js
+++ server/util.js
@@ -1,3 +1,6 @@
+let moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Seoul');
+
 exports.objectToQuerystring = (obj) => {
     return Object.keys(obj).filter((key) => obj[key] != undefined && obj[key] != '').reduce((str, key, i) => {
         let delimiter, val;
@@ -31,6 +34,14 @@ exports.UUID = () => {
     return partialUUID() + partialUUID() + '-' + partialUUID() + '-' + partialUUID() + '-' + partialUUID() + '-' + partialUUID() + partialUUID() + partialUUID();
 }
 
-exports.getYYYYMMDD = (target = new Date()) => {
-    return `${target.getFullYear()}${(target.getMonth()<9?'0'+target.getMonth():target.getMonth())}${target.getDate()<10?'0'+target.getDate():target.getDate()}`;
+exports.getYYYYMMDD = function (target = moment()) {
+    return target.format('YMMDD');
+    //return `${target.getFullYear()}${(target.getMonth()<9?'0'+(1+target.getMonth()):(1+target.getMonth()))}${target.getDate()<10?'0'+target.getDate():target.getDate()}`;
 }
+
+exports.getYYYYMMDDHH24MISS = function (target = moment()) {
+    return target.format('YMMDDHHmmss');
+    //return `${target.getFullYear()}${(target.getMonth()<9?'0'+(1+target.getMonth()):(1+target.getMonth()))}${target.getDate()<10?'0'+target.getDate():target.getDate()}${(target.getHours()<9?'0'+target.getHours():target.getHours())}${(target.getMinutes()<9?'0'+target.getMinutes():target.getMinutes())}${(target.getSeconds()<9?'0'+target.getSeconds():target.getSeconds())}`;
+}
+
+exports.moment = moment;
