f84b28a991e5bc54cece72dc73252a5e027ce136
iKoru <glassonyou@gmail.com>
2018-11-10 01:05:20 +0900

    complete candidate for scrapModel

--- server/models/scrapModel.js
+++ server/models/scrapModel.js
@@ -1,13 +1,87 @@
-const pool = require('./db').instance;
+const pool = require('./db').instance,
+    builder = require('./db').builder;
 
 exports.getScraps = async(userId, scrapGroupId, page = 1) => {
-
+    return await pool.executeQuery('getScraps',
+        builder.select()
+        .fields({
+            'DOCUMENT.DOCUMENT_ID': '"documentId"',
+            'BOARD_ID': '"boardId"',
+            'COMMENT_COUNT': '"commentCount"',
+            'VOTE_UP_COUNT': '"voteUpCount"',
+            'VOTE_DOWN_COUNT': '"voteDownCount"',
+            'VIEW_COUNT': '"viewCount"',
+            'WRITE_DATETIME': '"writeDateTime"',
+            'TITLE': '"title"',
+            'RESERVED1': '"reserved1"',
+            'RESERVED2': '"reserved2"',
+            'RESERVED3': '"reserved3"',
+            'RESERVED4': '"reserved4"'
+        })
+        .field(builder.case().when('IS_ANONYMOUS = true').then('익명').else(builder.rstr('USER_NICKNAME')), '"nickName"')
+        .from(builder.select().fields(['DOCUMENT_ID']).from('SS_HST_USER_SCRAP').where('USER_ID = ?', userId).where('SCRAP_GROUP_ID = ?', scrapGroupId), 'SUSER')
+        .join('SS_MST_DOCUMENT', 'DOCUMENT', 'DOCUMENT.DOCUMENT_ID = SUSER.DOCUMENT_ID')
+        .order('SUSER.DOCUMENT_ID', false)
+        .limit(10)
+        .offset((page - 1) * 10)
+        .toParam()
+    )
 }
 
 exports.createScrap = async(userId, scrapGroupId, documentId) => {
-
+    return await pool.executeQuery('createScrap',
+        builder.insert()
+        .into('SS_HST_USER_SCRAP')
+        .setFields({
+            'USER_ID': userId,
+            'SCRAP_GROUP_ID': scrapGroupId,
+            'DOCUMENT_ID': documentId
+        })
+        .toParam()
+    )
 }
 
 exports.deleteScrap = async(userId, scrapGroupId, documentId) => {
+    return await pool.executeQuery('deleteScrap',
+        builder.delete()
+        .from('SS_HST_USER_SCRAP')
+        .where('USER_ID = ?', userId)
+        .where('SCRAP_GROUP_ID = ?', scrapGroupId)
+        .where('DOCUMENT_ID = ?', documentId)
+        .toParam()
+    )
+}
 
+exports.createScrapGroup = async(userId, scrapGroupName) => {
+    return await pool.executeQuery('createScrapGroup',
+        builder.insert()
+        .into('SS_MST_USER_SCRAP_GROUP')
+        .setFields({
+            'USER_ID': userId,
+            'SCRAP_GROUP_ID': builder.str('SELECT MAX(COALESCE(SCRAP_GROUP_ID, 0)) + 1 FROM SS_MST_USER_SCRAP_GROUP WHERE USER_ID = ?', userId),
+            'SCRAP_GROUP_NAME': scrapGroupName
+        })
+        .returning('SCRAP_GROUP_ID', '"scrapGroupId"')
+        .toParam())
+}
+
+exports.updateScrapGroup = async(userId, scrapGroupId, scrapGroupName) => {
+    return await pool.executeQuery('updateScrapGroup',
+        builder.update()
+        .table('SS_MST_USER_SCRAP_GROUP')
+        .set('SCRAP_GROUP_NAME', scrapGroupName)
+        .where('USER_ID = ?', userId)
+        .where('SCRAP_GROUP_ID = ?', scrapGroupId)
+        .toParam()
+    )
+}
+
+exports.deleteScrapGroup = async(userId, scrapGroupId) => {
+    return await pool.executeQuery('deleteScrapGroup',
+        builder.delete()
+        .from('SS_MST_USER_SCRAP_GROUP')
+        .where('USER_ID = ?', userId)
+        .where('SCRAP_GROUP_ID = ?', scrapGroupId)
+        .toParam()
+    )
 }
