7c02d425175e96ea44bebbd1830eb7e35da5917e
iKoru <glassonyou@gmail.com>
2018-11-11 00:19:51 +0900

    add vote test expectation

--- test/voteModel.test.js
+++ test/voteModel.test.js
@@ -17,7 +17,8 @@ test('document and comment vote test', async() => {
     expect(document).toHaveProperty('rowCount', 1);
     expect(await voteModel.createDocumentVote('orange', document.rows[0].documentId, true)).toEqual(1);
     expect(await voteModel.createDocumentVote('orange', document.rows[0].documentId, false)).toHaveProperty('code', constants.dbErrorCode.PKDUPLICATION);
-
+    const document2 = (await documentModel.getDocument(document.rows[0].documentId))[0];
+    expect(document2.voteUpCount).toBeGreaterThan(0);
 
     const comment = (await commentModel.createComment({
         documentId: document.rows[0].documentId,
@@ -28,7 +29,8 @@ test('document and comment vote test', async() => {
     expect(comment).toHaveProperty('rowCount', 1);
     expect(await voteModel.createCommentVote('orange', comment.rows[0].commentId, true)).toEqual(1);
     expect(await voteModel.createCommentVote('orange', comment.rows[0].commentId, false)).toHaveProperty('code', constants.dbErrorCode.PKDUPLICATION);
-
+    const comment2 = (await commentModel.getComment(comment.rows[0].commentId))[0];
+    expect(comment2.voteUpCount).toBeGreaterThan(0);
     expect(await documentModel.deleteDocument(document.rows[0].documentId)).toEqual(1);
     expect(await commentModel.getComment(comment.rows[0].commentId)).toHaveLength(0);
 });
