7707f60a7350a70e3189f41b65a63fa4ae237698
koru <glassonyou@gmail.com>
2018-11-13 14:24:18 +0000

    divided server.js into two parts due to execute test

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const path = require('path');
+const express = require('express');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const compression = require('compression');
+
+const app = express()
+
+const router = require('./server/router');
+
+process.env.NODE_ENV = (process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production') ? 'production' : 'development';
+
+app.use(logger(process.env.NODE_ENV));
+app.use('/static', express.static(path.resolve(__dirname, 'client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+
+router(app); //take over to router
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 "use strict";
 
-const path = require('path');
-const express = require('express');
-
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const compression = require('compression');
-
-const app = express();
+const app = require('./app');
 const server = require("http").Server(app);
-
-const router = require('./server/router');
-
-process.env.NODE_ENV = (process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production') ? 'production' : 'development';
-
-app.use(logger(process.env.NODE_ENV));
-app.use('/static', express.static(path.resolve(__dirname, 'client')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(compression());
-
-router(app); //take over to router
 
 server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function () {
     const addr = server.address();
