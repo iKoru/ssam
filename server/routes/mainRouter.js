865f51c17eecd2df92aaddf56443f0e3ef405c25
koru <glassonyou@gmail.com>
2018-11-20 02:19:06 +0000

    routing error - temporary blocked

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const visitorOnly = require('../middlewares/visitorOnly'),
-    requiredSignin = require('../middlewares/requiredSignin');
+    requiredSignin = require('../middlewares/requiredSignin'),
+    {isNumeric} = require('../util'),
+    {reserved} = require('../constants');
 
 router.get('/index', visitorOnly('/'), (req, res) => {
     res.status(501).end();
@@ -17,18 +19,26 @@ router.get('/profile', requiredSignin, (req, res) => {
 router.get('/:boardId([a-zA-Z]+)', requiredSignin, (req, res, next) => {
     if (isNumeric(req.params.boardId) || reserved.includes(req.params.boardId)) {
         next();
+        return;
     }
+    console.log(req.route);
+    console.log('/:boardId targeted', req.params.boardId)
+    next();//temporary 
+    return;
     res.status(501).end();
 });
 
 router.get('/:boardId([a-zA-Z]+)/:documentId(^[\\d]+$)', requiredSignin, (req, res, next) => {
     if (reserved.includes(req.params.boardId)) {
         next();
+        return;
     }
+    console.log('/:boardId/:documentId targeted');
     res.status(501).end();
 });
 
 router.get('/:documentId(^[\\d]+$)', requiredSignin, (req, res, next) => {
+    console.log('/:documentId targeted');
     res.status(501).end();
 });
 
