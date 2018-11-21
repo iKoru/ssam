c6dde26ec370185244018b365bc3d8400476993f
iKoru <glassonyou@gmail.com>
2018-11-21 23:12:02 +0900

    fixed middleware duplilcated response problem

--- server/middlewares/checkSignin.js
+++ server/middlewares/checkSignin.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config.js');
 const util = require('../util'),
-logger = require('../logger');
+    logger = require('../logger');
 const userModel = require('../models/userModel')
 
 const auth = (req, res, next) => {
     const token = req.headers['x-auth'];
     if (!token) {
         next();
+        return;
     }
 
     const p = new Promise((resolve, reject) => {
         jwt.verify(token, config.jwtKey, config.jwtOptions, (err, result) => {
-            if (err){
+            if (err) {
                 reject(err);
                 return;
-            } 
+            }
             resolve(result);
         });
     })
@@ -24,18 +25,18 @@ const auth = (req, res, next) => {
         logger.error('로그인 에러(checkSignin) : ', error.message);
         next();
     }
-    p.then(async (result) => {
+    p.then(async(result) => {
         if (result.userId) {
             let user = await userModel.getUser(result.userId);
             if (user && user[0]) {
                 req.userObject = user[0];
                 next();
-            }else{
-                onError({message:'존재하지 않는 ID입니다.'});
+            } else {
+                onError({ message: '존재하지 않는 ID입니다.' });
                 return;
             }
-        }else{
-            onError({message:'비정상적인 접근입니다.'});
+        } else {
+            onError({ message: '비정상적인 접근입니다.' });
             return;
         }
     }).catch(onError)
