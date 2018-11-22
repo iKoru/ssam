a9da409ced21e35276fc1d393f84b5a9b5e31bf9
koru <glassonyou@gmail.com>
2018-11-22 05:43:01 +0000

    implemented client request for auth and its test case

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const requiredSignin = require('../middlewares/requiredSignin');
 const authModel = require('../models/authModel'),
+userModel = require('../models/userModel'),
     util = require('../util');
 //based on /auth
 router.get('/', requiredSignin, (req, res) => {
@@ -8,33 +9,46 @@ router.get('/', requiredSignin, (req, res) => {
 });
 
 router.post('/', requiredSignin, async (req, res) => {
+    if (req.userObject.status === 'AUTHORIZED' && req.userObject.emailVerifiedDate && util.moment(req.userObject.emailVerifiedDate, 'YYYYMMDD').add(11, 'months').isAfter(util.moment())) {
+        return res.status(403).json({message:'이미 인증을 받으셨습니다.'});
+    }
     let userId = req.userObject.userId;
     let email = req.userObject.email;
     let authKey;
-    let history = await authModel.getUserAuth(userId, true);
-    if (history && history.length > 0) {
-        authKey = history[0].authKey;
-        authModel.updateUserAuth({
+    let result = await authModel.getUserAuth(userId, 'NORMAL');
+    if (Array.isArray(result) && result.length > 0) {
+        authKey = result[0].authKey;
+        result = await authModel.updateUserAuth({
             userId: userId,
             authKey: authKey,
             sendDateTime: true
         })
     } else {
         authKey = util.UUID();
-        authModel.createUserAuth(userId, authKey);
+        result = await authModel.createUserAuth(userId, authKey);
     }
-    //send email
-
-    res.status(501).end();
+    //TODO : send email(async)
+    if(typeof result === 'number' && result>0){
+        return res.status(200).json({message:'등록된 이메일로 인증메일을 보내드렸습니다. 이메일을 확인해주세요.'});
+    }else{
+        return res.status(500).json({message:`인증 메일을 생성하는 도중에 오류가 발생했습니다.[${result.code}]`})
+    }
+    
 });
 
 router.get('/submit', async (req, res) => { // get /auth/submit 
     if (!req.query.userId || !req.query.authKey) {
-        res.status(404).json({ message: '잘못된 접근입니다.' }); //TODO : show failed page
+        return res.status(400).json({ message: '잘못된 접근입니다.' }); //TODO : show failed page
     } else {
         let userId = req.query.userId,
             authKey = req.query.authKey;
-        let history = await authModel.getUserAuth(userId, true);
+        const user = await userModel.getUser(userId);
+        if(!Array.isArray(user) || user.length < 1){
+            return res.status(404).json({message:'잘못된 접근입니다.'});
+        }else if(user[0].status !== 'NORMAL'){
+            return res.status(400).json({message:'이미 인증되어있거나, 인증을 진행할 수 없는 상태입니다. 관리자에게 문의해주세요.'});
+        }
+        let history = await authModel.getUserAuth(userId, 'NORMAL');
         if (history && history.length > 0) {
             let i = 0;
             while (i < history.length) {
@@ -44,16 +58,16 @@ router.get('/submit', async (req, res) => { // get /auth/submit
                         authKey: authKey,
                         status: 'DONE'
                     })
-                    if (userModel.updateUserAuth(userId) === 1) {
-                        res.status(200).json({ message: '정상적으로 인증되었습니다!' }); //TODO : show success page
+                    if (await userModel.updateUserAuth(userId) === 1 && await userModel.updateUserInfo({userId:userId, status:'AUTHORIZED'}) === 1) {
+                        return res.status(200).json({ message: '정상적으로 인증되었습니다!' }); //TODO : show success page
                     } else {
-                        res.status(500).json({ message: '인증사항을 저장하는 데 실패하였습니다. 관리자에게 문의해주세요.' }); //TODO : show failed page
+                        return res.status(500).json({ message: '인증사항을 저장하는 데 실패하였습니다. 관리자에게 문의해주세요.' }); //TODO : show failed page
                     }
                 }
                 i++;
             }
         }
-        res.status(404).json({ message: '잘못된 접근입니다.' }); //TODO : show failed page
+        return res.status(400).json({ message: '인증 기간이 만료되었습니다. 인증 이메일을 다시 요청해주세요.' }); //TODO : show failed page
     }
 });
 
--- test/routes/authRouter.test.js
+++ test/routes/authRouter.test.js
@@ -1,17 +1,78 @@
 /* global expect */
-const request = require('supertest')
-const app = require('../../app')
-
+const app = require('../../app'),
+ request = require('supertest')(app),
+ authModel = require('../../server/models/authModel'),
+ userModel = require('../../server/models/userModel')
+const headers = { 'Accept': 'application/json' };
 describe('Test the auth path', async () => {
-    test('auth pre test', async (done) => {
-        //response = await request(app).post('/signin').set('Accept', 'application/json').send({ userId: 'orange', password: 'xptmxm1!' });
-        //expect(response.statusCode).toBe(200);
-        //let jwt = response.body;
-        expect(1).toEqual(1);
+    test('auth mail send test', async (done) => {
+        let response = await request.post('/auth').set(headers);
+        expect(response.statusCode).toEqual(403);
+        
+        response = await request.post('/signin').set(headers).send({ userId: 'blue', password: 'xptmxm1!' });
+        expect(response.statusCode).toEqual(200);
+        let headers_local = {...headers, 'x-auth': response.body.token };
+        
+        response = await userModel.updateUserInfo({userId:'blue', status:'AUTHORIZED'});
+        expect(response).toEqual(1);
+        
+        response = await request.post('/auth').set(headers_local);
+        expect(response.statusCode).toEqual(403)
+        
+        response = await userModel.updateUserInfo({userId:'blue', status:'NORMAL'});
+        expect(response).toEqual(1);
+        
+        response = await request.post('/auth').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        
+        response = await authModel.getUserAuth('blue', 'NORMAL');
+        expect(response.length).toBeGreaterThan(0);
+        
         done();
     })
-    test('auth test', async (done) => {
-        expect(1).toEqual(1);
+    test('auth submit test', async (done) => {
+        let response = await userModel.getUser('blue');
+        expect(response.length).toEqual(1);
+        expect(response[0].status).toEqual('NORMAL');
+        
+        response = await authModel.getUserAuth('blue', 'NORMAL');
+        if(response.length <= 0){
+            response = await request.post('/signin').set(headers).send({ userId: 'blue', password: 'xptmxm1!' });
+            expect(response.statusCode).toEqual(200);
+            response = await request.post('/auth').set({...headers, 'x-auth':response.body.token});
+            expect(response.statusCode).toEqual(200);
+            response = await authModel.getUserAuth('blue', 'NORMAL');
+        }
+        expect(response.length).toBeGreaterThan(0);
+        const authKey = response[0].authKey;
+        
+        
+        response = await request.get('/auth/submit').set(headers).query({userId:'blue'});
+        expect(response.statusCode).toEqual(400);
+        
+        response = await request.get('/auth/submit').set(headers).query({authKey:authKey});
+        expect(response.statusCode).toEqual(400);
+        
+        response = await request.get('/auth/submit').set(headers).query({userId:'blue332', authKey:authKey});
+        expect(response.statusCode).toEqual(404);
+        response = await request.get('/auth/submit').set(headers).query({userId:'orange', authKey:authKey});
+        expect(response.statusCode).toEqual(400);
+        
+        response = await request.get('/auth/submit').set(headers).query({userId:'blue', authKey:authKey});
+        expect(response.statusCode).toEqual(200);
+        
+        response = await userModel.getUser('blue');
+        expect(response.length).toEqual(1);
+        expect(response[0].status).toEqual('AUTHORIZED');
+        
+        response = await request.get('/auth/submit').set(headers).query({userId:'blue', authKey:authKey});
+        expect(response.statusCode).toEqual(400);
+        
+        response = await userModel.updateUserInfo({userId:'blue', status:'NORMAL'});
+        expect(response).toEqual(1);
+        
+        response = await authModel.updateUserAuth({userId:'blue', authKey:authKey, status:'NORMAL'});
+        expect(response).toEqual(1);
         done();
     })
 
