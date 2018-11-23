29770589654b245a0ee40b9c7aad0248df362971
iKoru <glassonyou@gmail.com>
2018-11-23 16:18:08 +0900

    implemented get/delete document client request logic (candidate) - in progress

--- server/routes/documentRouter.js
+++ server/routes/documentRouter.js
@@ -1,16 +1,26 @@
 const router = require('express').Router();
-const requiredSignin = require('../middlewares/requiredSignin');
+const requiredAuth = require('../middlewares/requiredAuth'),
+    adminOnly = require('../middlewares/adminOnly');
 //based on /document
 
-router.post('/', requiredSignin, (req, res) => {
+router.post('/', requiredAuth, async(req, res) => {
     res.status(501).end();
 });
 
-router.put('/', requiredSignin, (req, res) => {
+router.put('/', requiredAuth, async(req, res) => {
     res.status(501).end();
 });
 
-router.delete('/:documentId(^[\\d]+$)', requiredSignin, (req, res) => {
-    res.status(501).end();
+router.delete('/:documentId(^[\\d]+$)', adminOnly, async(req, res) => {
+    let documentId = req.params.documentId;
+    if ((typeof documentId !== 'string' && typeof documentId !== 'number') || documentId === '') {
+        return res.status(400).json({ target: 'documentId', message: '요청을 수행하기 위해 필요한 정보가 없거나 올바르지 않습니다.' });
+    }
+    let result = await documentModel.deleteDocument(documentId);
+    if (typeof result === 'object' || result === 0) {
+        return res.status(500).json({ message: '게시물을 삭제하는 중에 오류가 발생했습니다.' });
+    } else {
+        return res.status(200).json({ message: '게시물을 삭제하였습니다.' });
+    }
 });
 module.exports = router;
--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const visitorOnly = require('../middlewares/visitorOnly'),
     requiredSignin = require('../middlewares/requiredSignin'),
-    {isNumeric} = require('../util'),
-    {reserved} = require('../constants');
+    { isNumeric } = require('../util'),
+    { reserved } = require('../constants');
+const documentModel = require('../models/documentModel');
 
 router.get('/index', visitorOnly('/'), (req, res) => {
     res.status(501).end();
@@ -17,29 +18,55 @@ router.get('/profile', requiredSignin, (req, res) => {
 });
 
 router.get('/:boardId([a-zA-Z]+)', requiredSignin, (req, res, next) => {
+    if (req.params.boardId === 'loungeBest' || req.params.boardId === 'topicBest') {
+
+    } else if (isNumeric(req.params.boardId) || reserved.includes(req.params.boardId)) {
+        next();
+        return;
+    } else {
+
+    }
+    console.log(req.route);
+    console.log('/:boardId targeted', req.params.boardId)
+    next(); //temporary 
+});
+
+router.get('/:boardId([a-zA-Z]+)/:documentId(^[\\d]+$)', requiredSignin, (req, res, next) => {
     if (isNumeric(req.params.boardId) || reserved.includes(req.params.boardId)) {
         next();
         return;
     }
-    console.log(req.route);
-    console.log('/:boardId targeted', req.params.boardId)
-    next();//temporary 
-    return;
-    res.status(501).end();
-});
-
-router.get('/:boardId([a-zA-Z]+)/:documentId(^[\\d]+$)', requiredSignin, (req, res, next) => {
-    if (reserved.includes(req.params.boardId)) {
-        next();
-        return;
+    let documentId = req.params.documentId;
+    if (!Number.isInteger(documentId)) {
+        documentId = parseInt(documentId);
+        if (isNaN(documentId)) {
+            next();
+            return;
+        }
     }
-    console.log('/:boardId/:documentId targeted');
-    res.status(501).end();
+    let result = await documentModel.getDocument(documentId);
+    if (Array.isArray(result) && result.length > 0) {
+        return res.status(200).json(result[0]);
+    } else {
+        return res.status(404).json({ target: 'documentId', message: '존재하지 않는 게시물입니다.' })
+    }
 });
 
 router.get('/:documentId(^[\\d]+$)', requiredSignin, (req, res, next) => {
-    console.log('/:documentId targeted');
-    res.status(501).end();
+    let documentId = req.params.documentId;
+    if (!Number.isInteger(documentId)) {
+        documentId = parseInt(documentId);
+        if (isNaN(documentId)) {
+            next();
+            return;
+        }
+    }
+    let result = await documentModel.getDocument(documentId);
+    if (Array.isArray(result) && result.length > 0) {
+        return res.status(200).json(result[0]);
+    } else {
+        return res.status(404).json({ target: 'documentId', message: '존재하지 않는 게시물입니다.' })
+    }
 });
 
 router.post('/survey', requiredSignin, (req, res) => {
