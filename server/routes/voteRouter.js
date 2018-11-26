9c719c1d086f762d8fe743938044449782cf65a7
iKoru <glassonyou@gmail.com>
2018-11-27 00:21:25 +0900

    clear import error

--- server/routes/voteRouter.js
+++ server/routes/voteRouter.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const requiredAuth = require('../middlewares/requiredAuth'),
-    voteModel = require('../models/voteModel');
+    voteModel = require('../models/voteModel'),
+    documentModel = require('../models/documentModel'),
+    commentModel = require('../models/commentModel'),
+    boardModel = require('../models/boardModel'),
+    logger = require('../logger');
 const { dbErrorCode } = require('../constants');
 //based on /vote
-router.post('/document', requiredAuth, (req, res) => {
+router.post('/document', requiredAuth, async(req, res) => {
     let documentId = req.body.documentId;
-    if (typeof comment.documentId === 'string') {
-        comment.documentId = parseInt(comment.documentId)
+    if (typeof documentId === 'string') {
+        documentId = Number(documentId)
     }
-    if (!Number.isInteger(comment.documentId)) {
+    if (!Number.isInteger(documentId) || documentId === 0) {
         return res.status(400).json({ target: 'documentId', message: '게시물을 찾을 수 없습니다.' });
     }
     const document = await documentModel.getDocument(documentId);
@@ -23,12 +27,8 @@ router.post('/document', requiredAuth, (req, res) => {
     }
 
     result = await voteModel.createDocumentVote(req.userObject.userId, documentId, true);
-    if (typeof result === 'number') {
-        if (result > 0) {
-            return res.status(200).json({ message: '추천했습니다.' });
-        } else {
-            return res.status(500).json({ message: '추천하지 못했습니다.' })
-        }
+    if (result.rowCount > 0 && result.rows && result.rows[0].voteUpCount >= 0) {
+        return res.status(200).json({ message: '추천했습니다.', voteUpCount: result.rows[0].voteUpCount });
     } else if (result.code === dbErrorCode.PKDUPLICATION) {
         return res.status(409).json({ message: '이미 추천하셨습니다.' });
     } else {
@@ -37,12 +37,12 @@ router.post('/document', requiredAuth, (req, res) => {
     }
 });
 
-router.post('/comment', requiredAuth, (req, res) => {
+router.post('/comment', requiredAuth, async(req, res) => {
     let commentId = req.body.commentId;
     if (typeof commentId === 'string') {
-        commentId = parseInt(commentId)
+        commentId = Number(commentId)
     }
-    if (!Number.isInteger(commentId)) {
+    if (!Number.isInteger(commentId) || commentId === 0) {
         return res.status(400).json({ target: 'commentId', message: '댓글을 찾을 수 없습니다.' });
     }
     const comment = await commentModel.getComment(commentId);
@@ -63,12 +63,8 @@ router.post('/comment', requiredAuth, (req, res) => {
     }
 
     result = await voteModel.createCommentVote(req.userObject.userId, commentId, true);
-    if (typeof result === 'number') {
-        if (result > 0) {
-            return res.status(200).json({ message: '추천했습니다.' });
-        } else {
-            return res.status(500).json({ message: '추천하지 못했습니다.' })
-        }
+    if (result.rowCount > 0 && result.rows && result.rows[0].voteUpCount >= 0) {
+        return res.status(200).json({ message: '추천했습니다.', voteUpCount: result.rows[0].voteUpCount });
     } else if (result.code === dbErrorCode.PKDUPLICATION) {
         return res.status(409).json({ message: '이미 추천하셨습니다.' });
     } else {
