b5d9a9a0c11f0f39171c9e74efb33d5b9ef471b2
koru <glassonyou@gmail.com>
2018-11-28 07:10:54 +0000

    created notification router and its test cases

---
+++ server/routes/notificationRouter.js
@@ -0,0 +1,192 @@
+const router = require('express').Router();
+const requiredSignin = require('../middlewares/requiredSignin'),
+    adminOnly = require('../middlewares/adminOnly'),
+    logger = require('../logger'),
+    { moment } = require('../util');
+const notificationModel = require('../models/notificationModel'),
+userModel = require('../models/userModel');
+//based on /notification
+
+router.get('/', requiredSignin, async(req, res) => {
+    let type = req.query.type;
+    if(typeof type !== 'string' || type.length > 2){
+        type = undefined;
+    }
+    let dateTimeBefore = req.query.dateTimeBefore;
+    if(typeof dateTimeBefore !== 'string' || !moment(dateTimeBefore, 'YYYYMMDDHH24MISS').isValid()){
+        dateTimeBefore = undefined;
+    }
+    
+    let result = await notificationModel.getNotifications(req.userObject.userId, dateTimeBefore, type)
+    if(Array.isArray(result)){
+        result.forEach(x=>{
+            x.message = x.template.replaceAll('$1', x.variable1);
+            x.message = x.message.replaceAll('$2', x.variable2);
+            x.message = x.message.replaceAll('$3', x.variable3);
+            x.message = x.message.replaceAll('$4', x.variable4);
+            delete x.template;
+            delete x.variable1;
+            delete x.variable2;
+            delete x.variable3;
+            delete x.variable4;
+            delete x.type;
+            delete x.target;
+        })
+        return res.status(200).json(result);
+    }else{
+        logger.error('알림 내용 조회 중 에러 : ', result, type, dateTimeBefore, req.userObject.userId)
+        return res.status(500).json({message:`알림 내용을 가져오지 못했습니다.[${result.code || ''}]`});
+    }
+});
+
+router.put('/', requiredSignin, async(req, res)=>{
+    let isAllClear = req.body.isAllClear, result;
+    if(isAllClear !== undefined && isAllClear === true){
+        result = await notificationModel.clearNotification(req.userObject.userId);
+    }else{
+        let notification = {
+            notificationId : req.body.notificationId,
+            isRead : req.body.isRead,
+            template:req.body.template,
+            variable1:req.body.variable1,
+            variable2:req.body.variable2,
+            variable3:req.body.variable3,
+            variable4:req.body.variable4,
+            href:req.body.href
+        }
+        if(typeof notification.notificationId === 'string'){
+            notification.notificationId = Number(notification.notificationId);
+        }
+        if(!Number.isInteger(notification.notificationId) || notification.notificationId === 0){
+            return res.status(400).json({target:'notificationId', message:'변경할 알림을 찾을 수 없습니다.'})
+        }
+        if(notification.isRead !== undefined && typeof notification.isRead !== 'boolean'){
+            return res.status(400).json({target:'isRead', message:'조회여부 값이 올바르지 않습니다.'})
+        }
+        if(!req.userObject.isAdmin){
+            if(notification.isRead === undefined){
+                return res.status(400).json({message:'변경할 내역이 없습니다.'});
+            }
+            delete notification.template;
+            delete notification.variable1;
+            delete notification.variable2;
+            delete notification.variable3;
+            delete notification.variable4;
+            delete notification.href;
+        }
+        result = await notificationModel.updateNotification(notification);
+    }
+    
+    if(typeof result === 'object'){
+        logger.error('알림 내역 변경 실패 : ', result, isAllClear);
+        return res.status(500).json({message:`알림 내역을 변경하지 못했습니다.[${result.code || ''}]`})
+    }else if(result === 0){
+        return res.status(404).json({target:'notificationId', message:'변경할 알림을 찾을 수 없습니다.'})
+    }else{
+        return res.status(200).json({message:'알림 내역을 변경하였습니다.'})
+    }
+})
+router.post('/', adminOnly, async(req, res) => {
+    let notification = {
+        type: req.body.type,
+        template: req.body.template,
+        variable1 : req.body.variable1,
+        variable2 : req.body.variable2,
+        variable3 : req.body.variable3,
+        variable4 : req.body.variable4,
+        userId : req.body.userId,
+        groupId : req.body.groupId,
+        href: req.body.href,
+        target: req.body.target
+    }
+    if (!['CC', 'DC', 'EV', 'AN'].includes(notification.type)) {
+        return res.status(400).json({ target: 'type', message: '알림 타입이 올바르지 않습니다.' })
+    }
+    if (typeof notification.template !== 'string' || notification.reportTypeDescription.length > 2000) {
+        return res.status(400).json({ target: 'template', message: '알림 내용 템플릿이 2000자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+    if (notification.variable1 !== undefined && (typeof notification.variable1 !== 'string' || notification.variable1.length > 100)) {
+        return res.status(400).json({ target: 'variable1', message: '알림 내용 변수1이 100자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+    if (notification.variable2 !== undefined && (typeof notification.variable2 !== 'string' || notification.variable2.length > 100)) {
+        return res.status(400).json({ target: 'variable2', message: '알림 내용 변수2가 100자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+    if (notification.variable3 !== undefined && (typeof notification.variable3 !== 'string' || notification.variable3.length > 100)) {
+        return res.status(400).json({ target: 'variable3', message: '알림 내용 변수3이 100자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+    if (notification.variable4 !== undefined && (typeof notification.variable4 !== 'string' || notification.variable4.length > 100)) {
+        return res.status(400).json({ target: 'variable4', message: '알림 내용 변수4가 100자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+    if (notification.userId !== undefined && typeof notification.userId !== 'string') {
+        return res.status(400).json({ target: 'userId', message: '알림을 보낼 사용자 ID가 올바르지 않습니다.' })
+    }
+    if (typeof notification.groupId === 'string') {
+        notification.groupId = Number(notification.groupId)
+    }
+    if(notification.groupId !== undefined && (!Number.isInteger(notification.groupId) || notification.groupId === 0)){
+        return res.status(400).json({target:'groupId', message:'알림을 보낼 그룹ID가 올바르지 않습니다.'})
+    }
+    if (notification.target !== undefined && typeof notification.target !== 'number') {
+        return res.status(400).json({ target: 'target', message: '알림 내용 타겟의 형태가 올바르지 않습니다.' })
+    }
+    if(notification.href !== undefined && (typeof notification.href !== 'string' || notification.href.length > 100)){
+        return res.status(400).json({ target: 'href', message: '알림 링크가 100자를 넘거나 형태가 올바르지 않습니다.' })
+    }
+
+    if(notification.groupId){
+        let users = await userModel.getUsers(null, null, null, notification.groupId);
+        if(Array.isArray(users)){
+            let i=0;
+            delete notification.userId;
+            users.forEach(x=>{
+                notificationModel.createNotification({...notification, userId:users[i].userId});
+            })
+            return res.status(200).json({message:'그룹 전체에 알림을 만들게 했습니다.'});
+        }else{
+            logger.error('알림 생성하기 위한 그룹별 유저 가져오기 중 에러 : ', users, notification.groupId);
+            return res.status(500).json({ message: `알림을 만들지 못했습니다.[${result.code || ''}]` })
+        }
+    }
+    let result = await notificationModel.createNotification(notification);
+    if (result.rowCount > 0 && result.rows && result.rows[0] && result.rows[0].notificationId > 0) {
+        return res.status(200).json({ message: '알림을 만들었습니다.', notificationId: result.rows[0].notificationId })
+    } else if (result.code) {
+        logger.error('알림 생성 시 에러 : ', result, notification);
+        return res.status(500).json({ message: `알림을 만들지 못했습니다.[${result.code || ''}]` })
+    } else {
+        logger.error('알림 생성 시 에러(2) : ', result, notification);
+        return res.status(500).json({ message: '알림을 만들지 못헀습니다...' });
+    }
+})
+
+router.delete('/:notificationId([0-9]+)', requiredSignin, async(req, res) => {
+    let notificationId = req.params.notificationId;
+    if (typeof notificationId === 'string') {
+        notificationId = Number(notificationId);
+    }
+    if (!Number.isInteger(notificationId) || notificationId === 0) {
+        return res.status(400).json({ target: 'notificationId', message: '삭제할 알림을 찾을 수 없습니다.' });
+    }
+    let result;
+    if(!req.userObject.isAdmin){
+        result = await notificationModel.getNotification(notificationId);
+        if(Array.isArray(result) && result.length > 0){
+            if(result[0].userId !== req.userObject.userId){
+                return res.status(403).json({target:'notificationId', message:'알림을 삭제할 수 있는 권한이 없습니다.'})
+            }
+        }else{
+            return res.status(404).json({target:'notificationId', message:'삭제할 알림을 찾을 수 없습니다.'});
+        }
+    }
+    
+    result = await notificationModel.deleteNotification(notificationId);
+    if (typeof result === 'object') {
+        logger.error('알림 삭제 중 에러 : ', result, req.userObject.userId, notificationId)
+        return res.status(500).json({ message: `알림을 삭제하던 중 오류가 발생했습니다.[${result.code || ''}]` });
+    }else if( result === 0){
+        return res.status(404).json({target:'notificationId', message:'삭제할 알림을 찾을 수 없습니다.'});
+    } else {
+        return res.status(200).json({ message: '알림을 삭제하였습니다.' });
+    }
+})
+module.exports = router;
---
+++ test/routes/notificationRouter.test.js
@@ -0,0 +1,92 @@
+/* global expect */
+const app = require('../../app'),
+    request = require('supertest')(app),
+    boardModel = require('../../server/models/boardModel'),
+    groupModel = require('../../server/models/groupModel'),
+    userModel = require('../../server/models/userModel'),
+    documentModel = require('../../server/models/documentModel'),
+    headers = { 'Accept': 'application/json' };
+    
+describe('Test the notification path', async () => {
+    test('notification crud test', async (done) => {
+        //create notification
+        let response = await request.get('/notification').set(headers);
+        expect(response.statusCode).toEqual(403);
+        
+        response = await request.post('/signin').set(headers).send({ userId: 'orange', password: 'xptmxm1!' });
+        expect(response.statusCode).toEqual(200);
+        let headers_local = {...headers, 'x-auth': response.body.token };
+        
+        response = await request.get('/notification').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        const originalLength = response.body.length;
+        
+        response = await request.post('/document').set(headers_local).send({boardId:'free', title:'notificationTest', contents:'notification test!!!', isAnonymous:true, allowAnonymous:true})
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toHaveProperty('documentId');
+        
+        let documentId = response.body.documentId;
+        expect(documentId).toBeGreaterThan(0);
+        
+        response = await request.post('/comment').set(headers_local).send({documentId:documentId, contents:'notification test comment!!!', isAnonymous:true})
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toHaveProperty('commentId');
+        let commentId = response.body.commentId;
+        expect(commentId).toBeGreaterThan(0);
+        
+        //no notification when create acomment on self-written document
+        response = await request.get('/notification').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.length).toEqual(originalLength);
+        
+        response = await request.post('/signin').set(headers).send({ userId: 'blue', password: 'xptmxm1!' });
+        expect(response.statusCode).toEqual(200);
+        let headers_local2 = {...headers, 'x-auth': response.body.token };
+        
+        //notification created when other user commented on its document
+        response = await request.post('/comment').set(headers_local2).send({documentId:documentId, contents:'notification test commentggg!!!', isAnonymous:true})
+        expect(response.statusCode).toEqual(200);
+        
+        response = await request.get('/notification').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        if(originalLength < 10){
+            expect(response.body.length).toEqual(originalLength + 1);
+        }
+        
+        //update notification when not read
+        response = await request.post('/comment').set(headers_local2).send({documentId:documentId, contents:'notification test commentg22gg!!!', isAnonymous:true})
+        expect(response.statusCode).toEqual(200);
+        
+        response = await request.get('/notification').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        if(originalLength < 10){
+            expect(response.body.length).toEqual(originalLength + 1);
+        }
+        
+        response = await request.post('/comment').set(headers_local2).send({documentId:documentId, parentCommentId:commentId, contents:'notification test child commentg22gg!!!', isAnonymous:true})
+        expect(response.statusCode).toEqual(200);
+        
+        response = await request.get('/notification').set(headers_local);
+        expect(response.statusCode).toEqual(200);
+        if(originalLength < 9){
+            expect(response.body.length).toEqual(originalLength + 2);
+        }
+        
+        //update notification
+        let notifications = response.body;
+        response = await request.put('/notification').set(headers_local).send({notificationId: notifications[0].notificationId, isRead:true})
+        expect(response.statusCode).toEqual(200);
+        
+        response = await request.put('/notification').set(headers_local).send({clearNotification:true})
+        expect(response.statusCode).toEqual(200);
+        
+        //delete notification
+        let i=0;
+        while(i<notifications.length){
+            response = await request.delete('/notification/'+notifications[i].notificationId).set(headers_local);
+            expect(response.statusCode).toEqual(200);
+        }
+        
+        done();
+    });
+})
