3f7ac27139976bbfe1d50b440166dd5e7aad3460
iKoru <glassonyou@gmail.com>
2018-12-12 00:06:12 +0900

    added feature to send notification to all users

--- server/models/notificationModel.js
+++ server/models/notificationModel.js
@@ -2,35 +2,35 @@ const pool = require('./db').instance,
     builder = require('./db').builder,
     { getYYYYMMDDHH24MISS } = require('../util');
 
-exports.getNotification = async(notificationId) => {
+exports.getNotification = async (notificationId) => {
     return await pool.executeQuery('getNotification',
         builder.select()
-        .fields({
-            'NOTIFICATION_ID': '"notificationId"',
-            'CREATED_DATETIME': '"createdDatetime"',
-            'TYPE': '"type"',
-            'USER_ID':'"userId"',
-            'TEMPLATE': '"template"',
-            'VARIABLE1': '"variable1"',
-            'VARIABLE2': '"variable2"',
-            'VARIABLE3': '"variable3"',
-            'VARIABLE4': '"variable4"',
-            'IS_READ': '"isRead"',
-            'HREF': '"href"'
-        })
-        .from('SS_HST_USER_NOTIFICATION')
-        .where('NOTIFICATION_ID = ?', notificationId)
-        .toParam()
+            .fields({
+                'NOTIFICATION_ID': '"notificationId"',
+                'CREATED_DATETIME': '"createdDatetime"',
+                'TYPE': '"type"',
+                'USER_ID': '"userId"',
+                'TEMPLATE': '"template"',
+                'VARIABLE1': '"variable1"',
+                'VARIABLE2': '"variable2"',
+                'VARIABLE3': '"variable3"',
+                'VARIABLE4': '"variable4"',
+                'IS_READ': '"isRead"',
+                'HREF': '"href"'
+            })
+            .from('SS_HST_USER_NOTIFICATION')
+            .where('NOTIFICATION_ID = ?', notificationId)
+            .toParam()
     );
 }
 
-exports.getNotifications = async(userId, datetimeBefore = null, type = null, target = null) => {
+exports.getNotifications = async (userId, datetimeBefore = null, type = null, target = null) => {
     let query = builder.select()
         .fields({
             'NOTIFICATION_ID': '"notificationId"',
             'CREATED_DATETIME': '"createdDatetime"',
             'TYPE': '"type"',
-            'USER_ID':'"userId"',
+            'USER_ID': '"userId"',
             'TEMPLATE': '"template"',
             'VARIABLE1': '"variable1"',
             'VARIABLE2': '"variable2"',
@@ -47,18 +47,18 @@ exports.getNotifications = async(userId, datetimeBefore = null, type = null, tar
     if (type) {
         query.where('TYPE = ?', type);
     }
-    if(target){
+    if (target) {
         query.where('TARGET = ?', target)
     }
-    return await pool.executeQuery('getNotifications' + (datetimeBefore ? 'date' : '') + (type ? 'type' : '') + (target?'target':''),
+    return await pool.executeQuery('getNotifications' + (datetimeBefore ? 'date' : '') + (type ? 'type' : '') + (target ? 'target' : ''),
         query.order('CREATED_DATETIME', false)
-        .order('IS_READ', true)
-        .limit(10)
-        .toParam()
+            .order('IS_READ', true)
+            .limit(10)
+            .toParam()
     );
 }
 
-exports.updateNotification = async(notification) => {
+exports.updateNotification = async (notification) => {
     let query = builder.update().table('SS_HST_USER_NOTIFICATION')
         .setFields({
             'TEMPLATE': (notification.template || builder.rstr('TEMPLATE')),
@@ -70,53 +70,123 @@ exports.updateNotification = async(notification) => {
             'HREF': (notification.href || builder.rstr('HREF'))
         })
         .where('NOTIFICATION_ID = ?', notification.notificationId)
-    if(notification.userId){
+    if (notification.userId) {
         query
-        .where('USER_ID = ?', notification.userId)
+            .where('USER_ID = ?', notification.userId)
     }
-    return await pool.executeQuery('updateNotification' + (notification.userId?'user':'all'),
+    return await pool.executeQuery('updateNotification' + (notification.userId ? 'user' : 'all'),
         query.toParam()
     );
 }
 
-exports.createNotification = async(notification) => {
-    return await pool.executeQuery('createNotification',
-        builder.insert()
-        .into('SS_HST_USER_NOTIFICATION')
-        .setFields({
-            'NOTIFICATION_ID': builder.rstr('CAST(nextval(\'SEQ_SS_HST_USER_NOTIFICATION\') AS INTEGER)'),
-            'USER_ID': notification.userId,
-            'CREATED_DATETIME': getYYYYMMDDHH24MISS(),
-            'TYPE': notification.type,
-            'TEMPLATE': notification.template,
-            'VARIABLE1': notification.variable1,
-            'VARIABLE2': notification.variable2,
-            'VARIABLE3': notification.variable3,
-            'VARIABLE4': notification.variable4,
-            'IS_READ': false,
-            'HREF': notification.href,
-            'TARGET':notification.target
-        })
-        .returning('NOTIFICATION_ID', '"notificationId"')
-        .toParam()
-    );
+exports.createNotification = async (notification, users) => {
+    if (users && users.length > 0) {//send target
+        return await pool.executeQuery('createNotificationG' + users.length,
+            builder.insert()
+                .into('SS_HST_USER_NOTIFICATION')
+                .setFieldsRows(
+                    users.map(x => {
+                        return {
+                            'NOTIFICATION_ID': builder.rstr('CAST(nextval(\'SEQ_SS_HST_USER_NOTIFICATION\') AS INTEGER)'),
+                            'USER_ID': x,
+                            'CREATED_DATETIME': getYYYYMMDDHH24MISS(),
+                            'TYPE': notification.type,
+                            'TEMPLATE': notification.template,
+                            'VARIABLE1': notification.variable1,
+                            'VARIABLE2': notification.variable2,
+                            'VARIABLE3': notification.variable3,
+                            'VARIABLE4': notification.variable4,
+                            'IS_READ': false,
+                            'HREF': notification.href,
+                            'TARGET': notification.target
+                        }
+                    })
+                )
+                .toParam()
+        );
+    } else if (!users && notification.userId) {
+        return await pool.executeQuery('createNotification',
+            builder.insert()
+                .into('SS_HST_USER_NOTIFICATION')
+                .setFields({
+                    'NOTIFICATION_ID': builder.rstr('CAST(nextval(\'SEQ_SS_HST_USER_NOTIFICATION\') AS INTEGER)'),
+                    'USER_ID': notification.userId,
+                    'CREATED_DATETIME': getYYYYMMDDHH24MISS(),
+                    'TYPE': notification.type,
+                    'TEMPLATE': notification.template,
+                    'VARIABLE1': notification.variable1,
+                    'VARIABLE2': notification.variable2,
+                    'VARIABLE3': notification.variable3,
+                    'VARIABLE4': notification.variable4,
+                    'IS_READ': false,
+                    'HREF': notification.href,
+                    'TARGET': notification.target
+                })
+                .returning('NOTIFICATION_ID', '"notificationId"')
+                .toParam()
+        );
+    } else if (!users && !notification.userId) {//send all
+        let array = ['NOTIFICATION_ID', 'USER_ID', 'CREATED_DATETIME', 'TEMPLATE', 'TYPE'],
+            query = builder.select();
+        query.field(builder.rstr('CAST(nextval(\'SEQ_SS_HST_USER_NOTIFICATION\') AS INTEGER)').toString(), 'NOTIFICATION_ID')
+            .field('USER_ID', 'USER_ID')
+            .field(`'${getYYYYMMDDHH24MISS()}'`, 'CREATED_DATETIME')
+            .field(`'${notification.template}'`, 'TEMPLATE')
+            .field(`'${notification.type}'`, 'TYPE')
+        if (notification.target) {
+            query.field(`'${notification.target}'`, 'TARGET');
+            array.push('TARGET')
+        }
+        if (notification.href) {
+            query.field(`'${notification.href}'`, 'HREF');
+            array.push('HREF')
+        }
+        if (notification.variable1) {
+            query.field(`'${notification.variable1}'`, 'VARIABLE1');
+            array.push('VARIABLE1');
+        }
+        if (notification.variable2) {
+            query.field(`'${notification.variable2}'`, 'VARIABLE2');
+            array.push('VARIABLE2');
+        }
+        if (notification.variable3) {
+            query.field(`'${notification.variable3}'`, 'VARIABLE3');
+            array.push('VARIABLE3');
+        }
+        if (notification.variable4) {
+            query.field(`'${notification.variable4}'`, 'VARIABLE4');
+            array.push('VARIABLE4');
+        }
+
+        return await pool.executeQuery(null,
+            builder.insert()
+                .into('SS_HST_USER_NOTIFICATION')
+                .fromQuery(array,
+                    query
+                        .from('SS_MST_USER')
+                        .where('STATUS <> \'BLOCKED\'')
+                        .where('STATUS <> \'DELETED\'')
+                )
+                .toParam()
+        );
+    }
 }
 
-exports.deleteNotification = async(notificationId) => {
+exports.deleteNotification = async (notificationId) => {
     return await pool.executeQuery('deleteNotification',
         builder.delete()
-        .from('SS_HST_USER_NOTIFICATION')
-        .where('NOTIFICATION_ID = ?', notificationId)
-        .toParam()
+            .from('SS_HST_USER_NOTIFICATION')
+            .where('NOTIFICATION_ID = ?', notificationId)
+            .toParam()
     );
 }
 
-exports.clearNotification = async(userId) => {
+exports.clearNotification = async (userId) => {
     return await pool.executeQuery('clearNotification',
         builder.update()
-        .table('SS_HST_USER_NOTIFICATION')
-        .set('IS_READ = true')
-        .where('USER_ID = ?', userId)
-        .toParam()
+            .table('SS_HST_USER_NOTIFICATION')
+            .set('IS_READ = true')
+            .where('USER_ID = ?', userId)
+            .toParam()
     )
 }
--- server/routes/notificationRouter.js
+++ server/routes/notificationRouter.js
@@ -4,22 +4,22 @@ const requiredSignin = require('../middlewares/requiredSignin'),
     logger = require('../logger'),
     { moment } = require('../util');
 const notificationModel = require('../models/notificationModel'),
-userModel = require('../models/userModel');
+    userModel = require('../models/userModel');
 //based on /notification
 
-router.get('/', requiredSignin, async(req, res) => {
+router.get('/', requiredSignin, async (req, res) => {
     let type = req.query.type;
-    if(typeof type !== 'string' || type.length > 2){
+    if (typeof type !== 'string' || type.length > 2) {
         type = undefined;
     }
     let dateTimeBefore = req.query.dateTimeBefore;
-    if(typeof dateTimeBefore !== 'string' || !moment(dateTimeBefore, 'YYYYMMDDHH24MISS').isValid()){
+    if (typeof dateTimeBefore !== 'string' || !moment(dateTimeBefore, 'YYYYMMDDHH24MISS').isValid()) {
         dateTimeBefore = undefined;
     }
-    
+
     let result = await notificationModel.getNotifications(req.userObject.userId, dateTimeBefore, type)
-    if(Array.isArray(result)){
-        result.forEach(x=>{
+    if (Array.isArray(result)) {
+        result.forEach(x => {
             x.message = x.template.replace(/\$1/gi, x.variable1);
             x.message = x.message.replace(/\$2/gi, x.variable2);
             x.message = x.message.replace(/\$3/gi, x.variable3);
@@ -33,39 +33,39 @@ router.get('/', requiredSignin, async(req, res) => {
             delete x.target;
         })
         return res.status(200).json(result);
-    }else{
+    } else {
         logger.error('알림 내용 조회 중 에러 : ', result, type, dateTimeBefore, req.userObject.userId)
-        return res.status(500).json({message:`알림 내용을 가져오지 못했습니다.[${result.code || ''}]`});
+        return res.status(500).json({ message: `알림 내용을 가져오지 못했습니다.[${result.code || ''}]` });
     }
 });
 
-router.put('/', requiredSignin, async(req, res)=>{
+router.put('/', requiredSignin, async (req, res) => {
     let clearNotification = req.body.clearNotification, result;
-    if(clearNotification !== undefined && clearNotification === true){
+    if (clearNotification !== undefined && clearNotification === true) {
         result = await notificationModel.clearNotification(req.userObject.userId);
-    }else{
+    } else {
         let notification = {
-            notificationId : req.body.notificationId,
-            isRead : req.body.isRead,
-            template:req.body.template,
-            variable1:req.body.variable1,
-            variable2:req.body.variable2,
-            variable3:req.body.variable3,
-            variable4:req.body.variable4,
-            href:req.body.href
+            notificationId: req.body.notificationId,
+            isRead: req.body.isRead,
+            template: req.body.template,
+            variable1: req.body.variable1,
+            variable2: req.body.variable2,
+            variable3: req.body.variable3,
+            variable4: req.body.variable4,
+            href: req.body.href
         }
-        if(typeof notification.notificationId === 'string'){
-            notification.notificationId = 1*notification.notificationId;
+        if (typeof notification.notificationId === 'string') {
+            notification.notificationId = 1 * notification.notificationId;
         }
-        if(!Number.isInteger(notification.notificationId) || notification.notificationId === 0){
-            return res.status(400).json({target:'notificationId', message:'변경할 알림을 찾을 수 없습니다.'})
+        if (!Number.isInteger(notification.notificationId) || notification.notificationId === 0) {
+            return res.status(400).json({ target: 'notificationId', message: '변경할 알림을 찾을 수 없습니다.' })
         }
-        if(notification.isRead !== undefined && typeof notification.isRead !== 'boolean'){
-            return res.status(400).json({target:'isRead', message:'조회여부 값이 올바르지 않습니다.'})
+        if (notification.isRead !== undefined && typeof notification.isRead !== 'boolean') {
+            return res.status(400).json({ target: 'isRead', message: '조회여부 값이 올바르지 않습니다.' })
         }
-        if(!req.userObject.isAdmin){
-            if(notification.isRead === undefined){
-                return res.status(400).json({message:'변경할 내역이 없습니다.'});
+        if (!req.userObject.isAdmin) {
+            if (notification.isRead === undefined) {
+                return res.status(400).json({ message: '변경할 내역이 없습니다.' });
             }
             delete notification.template;
             delete notification.variable1;
@@ -74,120 +74,125 @@ router.put('/', requiredSignin, async(req, res)=>{
             delete notification.variable4;
             delete notification.href;
             notification.userId = req.userObject.userId
-        }else{
+        } else {
             notification.userId = req.body.userId
         }
         result = await notificationModel.updateNotification(notification);
     }
-    
-    if(typeof result === 'object'){
+
+    if (typeof result === 'object') {
         logger.error('알림 내역 변경 실패 : ', result, clearNotification);
-        return res.status(500).json({message:`알림 내역을 변경하지 못했습니다.[${result.code || ''}]`})
-    }else if(result === 0){
-        return res.status(404).json({target:'notificationId', message:'변경할 알림을 찾을 수 없습니다.'})
-    }else{
-        return res.status(200).json({message:'알림 내역을 변경하였습니다.'})
+        return res.status(500).json({ message: `알림 내역을 변경하지 못했습니다.[${result.code || ''}]` })
+    } else if (result === 0) {
+        return res.status(404).json({ target: 'notificationId', message: '변경할 알림을 찾을 수 없습니다.' })
+    } else {
+        return res.status(200).json({ message: '알림 내역을 변경하였습니다.' })
     }
 })
-router.post('/', adminOnly, async(req, res) => {
+router.post('/', adminOnly, async (req, res) => {
     let notification = {
         type: req.body.type,
         template: req.body.template,
-        variable1 : req.body.variable1,
-        variable2 : req.body.variable2,
-        variable3 : req.body.variable3,
-        variable4 : req.body.variable4,
-        userId : req.body.userId,
-        groupId : req.body.groupId,
+        variable1: req.body.variable1,
+        variable2: req.body.variable2,
+        variable3: req.body.variable3,
+        variable4: req.body.variable4,
+        userId: req.body.userId,
+        groupId: req.body.groupId,
         href: req.body.href,
         target: req.body.target
-    }
+    };
     if (!['CC', 'DC', 'EV', 'AN'].includes(notification.type)) {
         return res.status(400).json({ target: 'type', message: '알림 타입이 올바르지 않습니다.' })
     }
-    if (typeof notification.template !== 'string' || notification.reportTypeDescription.length > 2000) {
+    if (typeof notification.template !== 'string' || notification.template.length > 2000) {
         return res.status(400).json({ target: 'template', message: '알림 내용 템플릿이 2000자를 넘거나 형태가 올바르지 않습니다.' })
     }
-    if (notification.variable1 !== undefined && (typeof notification.variable1 !== 'string' || notification.variable1.length > 100)) {
+    if (notification.variable1 && (typeof notification.variable1 !== 'string' || notification.variable1.length > 100)) {
         return res.status(400).json({ target: 'variable1', message: '알림 내용 변수1이 100자를 넘거나 형태가 올바르지 않습니다.' })
     }
-    if (notification.variable2 !== undefined && (typeof notification.variable2 !== 'string' || notification.variable2.length > 100)) {
+    if (notification.variable2 && (typeof notification.variable2 !== 'string' || notification.variable2.length > 100)) {
         return res.status(400).json({ target: 'variable2', message: '알림 내용 변수2가 100자를 넘거나 형태가 올바르지 않습니다.' })
     }
-    if (notification.variable3 !== undefined && (typeof notification.variable3 !== 'string' || notification.variable3.length > 100)) {
+    if (notification.variable3 && (typeof notification.variable3 !== 'string' || notification.variable3.length > 100)) {
         return res.status(400).json({ target: 'variable3', message: '알림 내용 변수3이 100자를 넘거나 형태가 올바르지 않습니다.' })
     }
-    if (notification.variable4 !== undefined && (typeof notification.variable4 !== 'string' || notification.variable4.length > 100)) {
+    if (notification.variable4 && (typeof notification.variable4 !== 'string' || notification.variable4.length > 100)) {
         return res.status(400).json({ target: 'variable4', message: '알림 내용 변수4가 100자를 넘거나 형태가 올바르지 않습니다.' })
     }
     if (notification.userId !== undefined && typeof notification.userId !== 'string') {
         return res.status(400).json({ target: 'userId', message: '알림을 보낼 사용자 ID가 올바르지 않습니다.' })
     }
     if (typeof notification.groupId === 'string') {
-        notification.groupId = 1*notification.groupId
+        notification.groupId = 1 * notification.groupId
     }
-    if(notification.groupId !== undefined && (!Number.isInteger(notification.groupId) || notification.groupId === 0)){
-        return res.status(400).json({target:'groupId', message:'알림을 보낼 그룹ID가 올바르지 않습니다.'})
+    if (notification.groupId !== undefined && (!Number.isInteger(notification.groupId) || notification.groupId === 0)) {
+        return res.status(400).json({ target: 'groupId', message: '알림을 보낼 그룹ID가 올바르지 않습니다.' })
     }
-    if (notification.target !== undefined && typeof notification.target !== 'number') {
-        return res.status(400).json({ target: 'target', message: '알림 내용 타겟의 형태가 올바르지 않습니다.' })
+    if (notification.target && typeof notification.target !== 'number') {
+        return res.status(400).json({ target: 'target', message: '알림 내용 타겟의 형태가 올바르지 않습니다.(숫자)' })
     }
-    if(notification.href !== undefined && (typeof notification.href !== 'string' || notification.href.length > 100)){
+    if (notification.href && (typeof notification.href !== 'string' || notification.href.length > 100)) {
         return res.status(400).json({ target: 'href', message: '알림 링크가 100자를 넘거나 형태가 올바르지 않습니다.' })
     }
 
-    if(notification.groupId){
+    if (notification.groupId) {
         let users = await userModel.getUsers(null, null, null, notification.groupId);
-        if(Array.isArray(users)){
-            let i=0;
+        if (Array.isArray(users)) {
+            let i = 0;
             delete notification.userId;
-            users.forEach(x=>{
-                notificationModel.createNotification({...notification, userId:users[i].userId});
-            })
-            return res.status(200).json({message:'그룹 전체에 알림을 만들게 했습니다.'});
-        }else{
+            notificationModel.createNotification(notification, users.map(x => x.userId));
+            return res.status(200).json({ message: '그룹 전체에 알림을 만들게 했습니다.' });
+        } else {
             logger.error('알림 생성하기 위한 그룹별 유저 가져오기 중 에러 : ', users, notification.groupId);
             return res.status(500).json({ message: `알림을 만들지 못했습니다.[${result.code || ''}]` })
         }
     }
-    let result = await notificationModel.createNotification(notification);
-    if (result.rowCount > 0 && result.rows && result.rows[0] && result.rows[0].notificationId > 0) {
-        return res.status(200).json({ message: '알림을 만들었습니다.', notificationId: result.rows[0].notificationId })
-    } else if (result.code) {
-        logger.error('알림 생성 시 에러 : ', result, notification);
-        return res.status(500).json({ message: `알림을 만들지 못했습니다.[${result.code || ''}]` })
+    console.log(notification);
+    let result;
+    if (notification.userId) {
+        result = await notificationModel.createNotification(notification);
+        if (result.rowCount > 0 && result.rows && result.rows[0] && result.rows[0].notificationId > 0) {
+            return res.status(200).json({ message: '알림을 만들었습니다.', notificationId: result.rows[0].notificationId })
+        } else if (result.code) {
+            logger.error('알림 생성 시 에러 : ', result, notification);
+            return res.status(500).json({ message: `알림을 만들지 못했습니다.[${result.code || ''}]` })
+        } else {
+            logger.error('알림 생성 시 에러(2) : ', result, notification);
+            return res.status(500).json({ message: '알림을 만들지 못헀습니다...' });
+        }
     } else {
-        logger.error('알림 생성 시 에러(2) : ', result, notification);
-        return res.status(500).json({ message: '알림을 만들지 못헀습니다...' });
+        result = notificationModel.createNotification(notification);
+        return res.status(200).json({ message: '회원 전체에 알림을 만들게 했습니다.' });
     }
 })
 
-router.delete('/:notificationId([0-9]+)', requiredSignin, async(req, res) => {
+router.delete('/:notificationId([0-9]+)', requiredSignin, async (req, res) => {
     let notificationId = req.params.notificationId;
     if (typeof notificationId === 'string') {
-        notificationId = 1*notificationId;
+        notificationId = 1 * notificationId;
     }
     if (!Number.isInteger(notificationId) || notificationId === 0) {
         return res.status(400).json({ target: 'notificationId', message: '삭제할 알림을 찾을 수 없습니다.' });
     }
     let result;
-    if(!req.userObject.isAdmin){
+    if (!req.userObject.isAdmin) {
         result = await notificationModel.getNotification(notificationId);
-        if(Array.isArray(result) && result.length > 0){
-            if(result[0].userId !== req.userObject.userId){
-                return res.status(403).json({target:'notificationId', message:'알림을 삭제할 수 있는 권한이 없습니다.'})
+        if (Array.isArray(result) && result.length > 0) {
+            if (result[0].userId !== req.userObject.userId) {
+                return res.status(403).json({ target: 'notificationId', message: '알림을 삭제할 수 있는 권한이 없습니다.' })
             }
-        }else{
-            return res.status(404).json({target:'notificationId', message:'삭제할 알림을 찾을 수 없습니다.'});
+        } else {
+            return res.status(404).json({ target: 'notificationId', message: '삭제할 알림을 찾을 수 없습니다.' });
         }
     }
-    
+
     result = await notificationModel.deleteNotification(notificationId);
     if (typeof result === 'object') {
         logger.error('알림 삭제 중 에러 : ', result, req.userObject.userId, notificationId)
         return res.status(500).json({ message: `알림을 삭제하던 중 오류가 발생했습니다.[${result.code || ''}]` });
-    }else if( result === 0){
-        return res.status(404).json({target:'notificationId', message:'삭제할 알림을 찾을 수 없습니다.'});
+    } else if (result === 0) {
+        return res.status(404).json({ target: 'notificationId', message: '삭제할 알림을 찾을 수 없습니다.' });
     } else {
         return res.status(200).json({ message: '알림을 삭제하였습니다.' });
     }
