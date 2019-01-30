9ca9b3af5a1f555c204a84becccc7d28ac300cf1
koru <glassonyou@gmail.com>
2019-01-30 08:31:36 +0000

    remove logging

--- app.js
+++ app.js
@@ -29,10 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
-app.use((req,res,next) => {
-    console.log(req)
-    next()
-})
+
 router(app); //take over to router
 //at last, take error to error handler
 app.use((err, req, res, next) => {
